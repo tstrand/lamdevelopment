f94e29086571c55a6fc5067c1da213c9fe84b854
tylermstrand@gmail.com <tylermstrand@gmail.com>
2017-02-07 00:46:55 -0500

    Updated Faq and 9 other files

--- js/main.js
+++ js/main.js
@@ -22,6 +22,17 @@
         }
     );
 
+    // Javascript to enable link to tab
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-pills a[href="#' + url.split('#')[1] + '"]').tab('show');
+    } 
+
+    // Change hash for page-reload
+    $('.nav-pills a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+    })
+
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
